3e913dc6340254c62e1e56919ef93c8a1bf1d525
Michael Havart <michael.havart@gmail.com>
2013-05-28 18:01:52 -0400

    updated webBrowser

--- src/nuke/tools/webBrowser.py
+++ src/nuke/tools/webBrowser.py
@@ -11,36 +11,36 @@ class WebBrowser(QWidget):
         QWidget.__init__(self)
         self.webView = QWebView()
 
-        self.setLayout( QVBoxLayout() )  
+        self.setLayout(QVBoxLayout())  
 
-        self.locationEdit = QLineEdit( 'http://www.google.com' )
-        self.locationEdit.setSizePolicy( QSizePolicy.Expanding, self.locationEdit.sizePolicy().verticalPolicy() )
+        self.locationEdit = QLineEdit('http://www.google.com')
+        self.locationEdit.setSizePolicy(QSizePolicy.Expanding, self.locationEdit.sizePolicy().verticalPolicy())
 
-        QObject.connect( self.locationEdit, SIGNAL('returnPressed()'),  self.changeLocation )
-        QObject.connect( self.webView,   SIGNAL('urlChanged(QUrl)'),     self.urlChanged )
+        QObject.connect(self.locationEdit, SIGNAL('returnPressed()'), self.changeLocation)
+        QObject.connect(self.webView, SIGNAL('urlChanged(QUrl)'), self.urlChanged)
 
-        self.layout().addWidget( self.locationEdit )
+        self.layout().addWidget(self.locationEdit)
 
         bar = QToolBar()
-        bar.addAction( self.webView.pageAction(QWebPage.Back))
-        bar.addAction( self.webView.pageAction(QWebPage.Forward))
-        bar.addAction( self.webView.pageAction(QWebPage.Stop))
-        bar.addAction( self.webView.pageAction(QWebPage.Reload))
+        bar.addAction(self.webView.pageAction(QWebPage.Back))
+        bar.addAction(self.webView.pageAction(QWebPage.Forward))
+        bar.addAction(self.webView.pageAction(QWebPage.Stop))
+        bar.addAction(self.webView.pageAction(QWebPage.Reload))
         bar.addSeparator()
 
-        self.layout().addWidget( bar )
-        self.layout().addWidget( self.webView )
+        self.layout().addWidget(bar)
+        self.layout().addWidget(self.webView)
 
         url = 'http://www.google.com/' 
-        self.webView.load( QUrl( url ) )
-        self.locationEdit.setText( url ) 
-        self.setSizePolicy( QSizePolicy( QSizePolicy.Expanding,  QSizePolicy.Expanding))
+        self.webView.load(QUrl(url))
+        self.locationEdit.setText(url) 
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding))
 
     def changeLocation(self):
         url = self.locationEdit.text()
-        if not url.startswith( 'http://' ):
+        if not url.startswith('http://'):
             url = 'http://' + url
-        self.webView.load( QUrl(url) )
+        self.webView.load(QUrl(url))
 
     def urlChanged(self, url):
-        self.locationEdit.setText( url.toString() )
+        self.locationEdit.setText(url.toString())
